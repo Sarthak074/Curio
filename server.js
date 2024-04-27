57db6e73b2dc3a56c31538a97e6327e66d6caab9
Sarthak Bhandare <94851031+Sarthak074@users.noreply.github.com>
2024-04-27 09:55:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 const MODEL_NAME = "gemini-pro";
-const API_KEY = process.env.API_KEY;
+const API_KEY = "AIzaSyA8aTYJkqq7DYtrEkTemkn7EOV6HMVvERg";
 
 async function runChat(userInput) {
   const genAI = new GoogleGenerativeAI(API_KEY);
